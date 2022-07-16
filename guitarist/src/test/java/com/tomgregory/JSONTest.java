8bc73c3ebe00cc5471dddc6e3489b9db3c3ab03f
Tom Gregory <t.k.gregory@gmail.com>
2022-07-16 11:50:40 +0100

    Added test for ofNullable.

--- guitarist/src/test/java/com/tomgregory/JSONTest.java
+++ guitarist/src/test/java/com/tomgregory/JSONTest.java
@@ -3,8 +3,11 @@ package com.tomgregory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -13,11 +16,32 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JSONTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
     @AllArgsConstructor
     @Getter
     private static final class Guitarist implements Serializable {
         private String lastName;
         private Optional<Integer> ageAtDeath;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    private static final class BetterGuitarist implements Serializable {
+        private String lastName;
+        private Integer ageAtDeath;
     }
 
     @Test
@@ -47,5 +71,32 @@ public class JSONTest {
         String json = objectMapper.writeValueAsString(new Guitarist("Knopfler", Optional.empty()));
 
         assertEquals("{\"lastName\":\"Knopfler\",\"ageAtDeath\":null}", json);
+    }
+
+    @Test
+    public void serializesToJSONUsingNonOptionalClassVariable() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String json = objectMapper.writeValueAsString(BetterGuitarist.builder().lastName("Knopfler").build());
+
+        assertEquals("{\"lastName\":\"Knopfler\",\"ageAtDeath\":null}", json);
+    }
+
+    @Test
+    public void ofNullableWithValue() {
+        BetterGuitarist guitarist = BetterGuitarist.builder().lastName("Hendrix").ageAtDeath(27).build();
+
+        Optional.ofNullable(guitarist.getAgeAtDeath()).ifPresentOrElse(age -> System.out.println("Died at age " + age), () -> System.out.println("Still alive!"));
+
+        assertEquals("Died at age 27", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void ofNullableWithNull() {
+        BetterGuitarist guitarist = BetterGuitarist.builder().lastName("Knopfler").build();
+
+        Optional.ofNullable(guitarist.getAgeAtDeath()).ifPresentOrElse(age -> System.out.println("Died at age " + age), () -> System.out.println("Still alive!"));
+
+        assertEquals("Still alive!", outputStreamCaptor.toString().trim());
     }
 }
